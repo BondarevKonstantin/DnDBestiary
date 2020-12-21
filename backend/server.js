dc5b3962c5f3d61be56fe41280a8991192afba21
Konstantin <Bondkr@mail.ru>
2020-12-21 10:01:03 +0300

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
 import connectDB from "./config/db.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
@@ -15,13 +16,24 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("API is running")
-})
-
 app.use("/api/creatures", creatureRoutes)
 app.use("/api/spells", spellRoutes)
 app.use("/api/users", userRoutes)
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === "production") {
+  console.log(path.resolve(__dirname, "frontend", "build"))
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running")
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
